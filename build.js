851a7a9e2dcb808eb6e04d1d45cd22fd68d7116d
kingmicon <uajirimicon@gmail.com>
2023-11-24 16:20:59 +0100

    correct1

---
+++ build.js
@@ -0,0 +1,3 @@
+const fs = require("fs");
+
+fs.writeFileSync("./netlify/functions/submitForm.js", "Hello, Netlify Functions!")
--- src/components/Alertcontex.js
+++
@@ -1,9 +0,0 @@
-import React, {createContext} from 'react'
-
-const Alertcontex = () => {
-  return (
-    <div>Alertcontex</div>
-  )
-}
-
-export default Alertcontex
