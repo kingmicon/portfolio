2fd711c7803cd931e00246c9724e2067d5e31930
kingmicon <uajirimicon@gmail.com>
2023-11-24 20:15:20 +0100

    correct4

--- build.js
+++ build.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
 
+fs.writeFileSync("./netlify/functions/submitForm.js", "Hello, Netlify Functions!")
+
 const fileContent = fs.readFileSync("./netlify/functions/submitForm.js", "utf-8");
 console.log("File Content:", fileContent);
