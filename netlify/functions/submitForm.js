9167def873760865d6c01e05718a1d79c9e65294
kingmicon <uajirimicon@gmail.com>
2023-11-24 17:03:32 +0100

    correct2

---
+++ netlify/functions/submitForm.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+exports.handler = async (event, context) => {
+    const { name, email, message} = JSON.parse (event.body);
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user:"uajirimicon@gmail.com",
+            pass: "utiajirimicon1995"
+        },
+    });
+    const mailOptions = {
+        from: "uajirimicon@gmail.com",
+        to: "uajirimicon@gmail.com",
+        subject: "New Form Submission",
+        text: `name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "request submitted successfully!"}),
+    };
+};
