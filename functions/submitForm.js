4b0cd848935d466c862abca12862ee99cec31875
kingmicon <uajirimicon@gmail.com>
2023-11-25 00:28:56 +0100

    correct4

--- functions/submitForm.js
+++ functions/submitForm.js
@@ -1,22 +1,18 @@
-const fs = require("fs");
-
-const fileContent = fs.readFileSync("./netlify/functions/submitForm.js", "utf-8");
-console.log("File Content:", fileContent);
 const nodemailer = require("nodemailer");
 
 exports.handler = async (event, context) => {
-    const { name, email, message} = JSON.parse (event.body);
+    const { name, email, message} = JSON.parse(event.body);
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
             user:"uajirimicon@gmail.com",
-            pass: "utiajirimicon1995"
+            pass: "utiajirimicon1995",
         },
     });
     const mailOptions = {
         from: "uajirimicon@gmail.com",
         to: "uajirimicon@gmail.com",
-        subject: "New Form Submission",
+        subject: "JOB REQUEST",
         text: `name: ${name}\nEmail: ${email}\nMessage: ${message}`,
     };
 
@@ -24,6 +20,6 @@ exports.handler = async (event, context) => {
 
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "request submitted successfully!"}),
+        body: JSON.stringify({ message: "request submitted successfully!"}),
     };
 };
