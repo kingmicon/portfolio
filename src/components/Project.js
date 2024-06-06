47897eaa2c34d0a86ef3ebd3c49c3d4556e805e8
kingmicon <uajirimicon@gmail.com>
2024-06-06 17:31:31 +0100

    project update

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,6 +13,26 @@ const Project = () => {
       <AnimationOnScroll animateIn="animate__zoomIn" animateOut="animate__zoomOut">
       <div className='project-works'>
         <div className='projects-text'>
+          <h1>Doctor DashBoard</h1>
+          <p>This is a web app that showcase the dashboard of a doctor which gives him details of patients. This sites has only desktop view and was done with ReactJs, chatjs and plain CSS</p>
+          <button><a href= "https://coalationdashboard.netlify.app" target="_blank" rel="noopener noreferrer">view Demo</a></button>
+          <button><a href="https://github.com/kingmicon/drDashboard.git" target="_blank" rel="noopener noreferrer">Source Code</a></button>
+        </div>
+      </div>
+      </AnimationOnScroll>
+      <AnimationOnScroll animateIn="animate__zoomIn" animateOut="animate__zoomOut">
+      <div className='project-works'>
+        <div className='projects-text'>
+          <h1>Customer DashBoard</h1>
+          <p>This is a web app that displays the the profile of a customer with history of his transactions and also details of his money spent. This sites was done with plain CSS, ReactJs and chatJs</p>
+          <button><a href= "https://662e6ebf5231bc000833903b--onfleekqdashboard.netlify.app/" target="_blank" rel="noopener noreferrer">view Demo</a></button>
+          <button><a href="https://github.com/kingmicon/onfleekq-app.git" target="_blank" rel="noopener noreferrer">Source Code</a></button>
+        </div>
+      </div>
+      </AnimationOnScroll>
+      <AnimationOnScroll animateIn="animate__zoomIn" animateOut="animate__zoomOut">
+      <div className='project-works'>
+        <div className='projects-text'>
           <h1>Solar Web</h1>
           <p>This is a web sites that showcases the different solar bodies. This sites was done with Tailwind CSS and ReactJs</p>
           <button><a href= "https://admirable-dusk-36e2db.netlify.app/" target="_blank" rel="noopener noreferrer">view Demo</a></button>
@@ -34,7 +54,7 @@ const Project = () => {
       <div className='project-works'>
         <div className='projects-text'>
           <h1>Landing Page</h1>
-          <p>This is a landing page for a website. This website was created with ReactJs and CSS. it has only desktop view</p>
+          <p>This is a landing page for a website. This website was created with ReactJs and CSS.</p>
           <button><a href= "https://eazipayappclone.netlify.app/" target="_blank" rel="noopener noreferrer">view Demo</a></button>
           <button><a href= "https://github.com/kingmicon/eaziappclone.git" target="_blank" rel="noopener noreferrer">Source Code</a></button>
         </div>
